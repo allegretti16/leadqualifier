13d67bacef858ec1c31e2d3f9855ccf70b3423e3
Isidoro Allegretti <allegretti@extendi.it>
2025-04-01 14:07:07 +0200

    nocors

--- server.js
+++ server.js
@@ -11,19 +11,6 @@ const openai = new OpenAI({
 
 const app = express();
 
-// Middleware per CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
